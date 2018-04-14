0b54a3ca7bc47753efdafd80419df4df4e70298f
Ehtesham Siddiqui <ehtesham@bibliocommons.com>
2018-04-14 17:39:08 -0400

    Initial commit

---
+++ src/main/java/com/es/coda/rogersrouter/RogersRouterApplication.java
@@ -0,0 +1,12 @@
+package com.es.coda.rogersrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogersRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogersRouterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/es/coda/rogersrouter/RogersRouterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.es.coda.rogersrouter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RogersRouterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
